1d09e8b3beb623ccee28ef415308412af3295f27
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-05 18:18:16 +0330

    Add Console Project

---
+++ Blockchain_v1/BC_Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using BC_Library.Dto;
+using BC_Library.Service;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BlockService myCoin = new BlockService();
+
+        Console.WriteLine("Mining block 1...");
+        BlockDto block1 = new BlockDto(1, "Amount: 50", myCoin.GetLatestBlock().Hash);
+        CommonService.MineBlock(block1, 3);
+        myCoin.AddBlock(block1);
+
+        Console.WriteLine("Mining block 2...");
+        BlockDto block2 = new BlockDto(2, "Amount: 100", myCoin.GetLatestBlock().Hash);
+        CommonService.MineBlock(block2, 3);
+        myCoin.AddBlock(block2);
+
+        Console.WriteLine("Is blockchain valid? " + myCoin.IsChainValid());
+    }
+}
