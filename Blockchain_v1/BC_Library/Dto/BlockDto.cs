84b2880cad2529b715ebeed1e1eda72c4a0c8bf2
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-06 16:45:02 +0330

    Update BlockDto

--- Blockchain_v1/BC_Library/Dto/BlockDto.cs
+++ Blockchain_v1/BC_Library/Dto/BlockDto.cs
@@ -14,24 +14,25 @@ namespace BC_Library.Dto
         public string PreviousHash { get; set; }
         public string Hash { get; set; }
         public int Nonce { get; set; }
+        public int Difficulty { get; set; }
 
-        public BlockDto(int index, List<TransactionDto> transactions, string previousHash)
+        public BlockDto(int index, List<TransactionDto> transactions, string previousHash, int difficulty)
         {
             Index = index;
             Timestamp = DateTime.UtcNow;
             Transactions = transactions;
             PreviousHash = previousHash;
             Nonce = 0;
+            Difficulty = difficulty;
             Hash = CalculateHash();
         }
         public string CalculateHash()
         {
             string txData = JsonSerializer.Serialize(Transactions);
-            string rawData = Index + Timestamp.ToString() + txData + PreviousHash + Nonce;
+            string rawData = Index + Timestamp.ToString("o") + txData + PreviousHash + Nonce + Difficulty;
             using var sha265 = SHA256.Create();
             var bytes = sha265.ComputeHash(Encoding.UTF8.GetBytes(rawData));
             return Convert.ToBase64String(bytes);
         }
-
     }
 }
