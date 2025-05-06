72dcb28fe5fc3a64a89d27be0becb5a545021c84
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-06 17:21:46 +0330

    Add WalletDto

---
+++ Blockchain_v1/BC_Library/Dto/WalletDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BC_Library.Dto
+{
+    public class WalletDto
+    {
+        public string PublicKey { get; set; }
+        public RSA PrivateKey{ get; set; }
+        public WalletDto()
+        {
+            using var rsa = RSA.Create(2048);
+            PublicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+            PrivateKey = RSA.Create();
+            PrivateKey.ImportRSAPrivateKey(rsa.ExportRSAPrivateKey(), out _);
+            
+        }
+        public TransactionDto CreateTransaction(string toAddress, decimal amount)
+        {
+            var tx = new TransactionDto(PublicKey, toAddress, amount);
+            tx.SignTransaction(PrivateKey);
+            return tx;
+        }
+    }
+}
