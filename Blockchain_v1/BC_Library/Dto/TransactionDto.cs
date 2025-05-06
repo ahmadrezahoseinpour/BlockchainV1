5b5e0eb66a2a6f6e899390457046efbc348b4e96
ahmadrezahoseinpour <ahmadrezahp13802@gmail.com>
2025-05-06 11:57:49 +0330

    Add Transaction Dto

---
+++ Blockchain_v1/BC_Library/Dto/TransactionDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BC_Library.Dto
+{
+    public class TransactionDto
+    {
+        public string FromAddress { get; set; }
+        public string ToAddress { get; set; }
+        public decimal Amount { get; set; }
+
+        public TransactionDto(string from, string to, decimal amount)
+        {
+            FromAddress = from;
+            ToAddress = to;
+            Amount = amount;
+        }
+    }
+
+}
